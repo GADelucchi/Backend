58ba27b6269393a9b36f92fca371de97012927f6
Gastón A. Delucchi <gaston@MacBook-Pro-de-Gaston.local>
2023-03-19 19:00:40 -0300

    first commit

---
+++ clases/clase-1/script.js
@@ -0,0 +1,15 @@
+console.log('hola mundo');
+// datos primitivos: string, number, null, undefined, boolean
+
+let productos = [
+    {id: 1,
+    nombre: 'Remera',
+    precio: 1000,
+    stock: 20},
+    {id: 2,
+    nombre: 'Pantalón',
+    precio: 2000,
+    stock: 10}
+]
+
+console.table(productos)
---
+++ desafios-entregables/clase-3/script.js
@@ -0,0 +1,44 @@
+// Creamos la clase
+class ProductManager {
+    // Definimos su constructor
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+    }
+    // // Definimos las variables estáticas
+    static products = []
+    // Definimos los métodos
+    // Método para agregar productos
+    addProduct(title, description, price, thumbnail, code, stock){
+        ProductManager.products.push(new ProductManager(title, description, price, thumbnail, code, stock,))
+    }
+    // Método para mostrar los productos
+    getProducts(){
+        return ProductManager.products
+    }
+    // Método para buscar un producto por ID
+    getProductById(ProductId){
+        if (ProductId === this.code) {
+            console.log('hola');
+        } else {
+            console.log('Not Found');
+        }
+    }
+}
+
+// Instancia de la clase
+const producto = new ProductManager()
+// Prueba del método getProducts
+console.log(producto.getProducts());
+// Prueba del método addProduct y revisión de correcto agregado al array
+producto.addProduct('Producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+console.log(producto.getProducts());
+// Prueba del ID autoincrementable
+// producto.addProduct('Producto prueba 2', 'Este es el segundo producto prueba', 100, 'Sin imagen', 'abc223', 1);
+// console.log(producto.getProducts());
+// Prueba del método getProductById
+producto.getProductById('abc123')
