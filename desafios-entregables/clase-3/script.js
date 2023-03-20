3fdac00d73f409f9ff6cb06fe18d54cfd9700115
Gastón A. Delucchi <gaston@MacBook-Pro-de-Gaston.local>
2023-03-20 20:06:12 -0300

    Desafío entregable terminado

--- desafios-entregables/clase-3/script.js
+++ desafios-entregables/clase-3/script.js
@@ -1,31 +1,52 @@
 // Creamos la clase
 class ProductManager {
     // Definimos su constructor
-    constructor(title, description, price, thumbnail, code, stock) {
-        this.title = title
-        this.description = description
-        this.price = price
-        this.thumbnail = thumbnail
-        this.code = code
-        this.stock = stock
-        this.id = 1
+    constructor() {
+        this.products = []
     }
-    // // Definimos las variables estáticas
-    static products = []
+
     // Definimos los métodos
-    // Método para agregar productos
+    // Método para agregar productos y validar los campos
     addProduct(title, description, price, thumbnail, code, stock){
-        ProductManager.products.push(new ProductManager(title, description, price, thumbnail, code, stock,))
-        this.id++
+        // Datos del producto
+        const product = {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        id: this.products.length +1
+        }
+
+        // Validaciones
+        if (
+            !product.title ||
+            !product.description ||
+            !product.price ||
+            !product.thumbnail ||
+            !product.code ||
+            !product.stock
+        ) {
+            console.log(`Todos los campos son obligatorios`);
+        } else if (
+            this.products.some((stock) =>  stock.code === product.code)
+        ) {
+            console.log(`No se permiten códigos repetidos`);
+        } else {
+            this.products.push(product)
+            console.log(`Producto agregado correctamente`);
+        }
     }
+
     // Método para mostrar los productos
     getProducts(){
-        return ProductManager.products
+        return console.log(this.products)
     }
     // Método para buscar un producto por ID
     getProductById(productId){
-        if(ProductManager.products.some(product => product.code === productId)){
-            console.log(`Producto con código ${productId}`);
+        if(this.products.some(product => product.code === productId)){
+            console.log(`Producto con código ${productId} encontrado`);
         } else {
             console.log(`Not found`);
         }
@@ -35,13 +56,19 @@ class ProductManager {
 // Instancia de la clase
 const producto = new ProductManager()
 // Prueba del método getProducts
-console.log(producto.getProducts());
+producto.getProducts();
 // Prueba del método addProduct y revisión de correcto agregado al array
 producto.addProduct('Producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
-console.log(producto.getProducts());
+producto.getProducts();
 // Prueba del ID autoincrementable
 producto.addProduct('Producto prueba 2', 'Este es el segundo producto prueba', 100, 'Sin imagen', 'abc223', 1);
-console.log(producto.getProducts());
+producto.getProducts();
+// Prueba de validación de propiedades
+producto.addProduct('Producto prueba 3', 'Este es el tercer producto prueba', 50, 'Sin imagen', 'abc333');
+producto.getProducts();
+// Prueba de validación de código repetido
+producto.addProduct('Producto prueba 4', 'Este es el cuarto producto prueba', 1000, 'Sin imagen', 'abc123', 3);
+producto.getProducts();
 // Prueba del método getProductById
 producto.getProductById('abc123')
 // Forzamos la prueba del método getProductById
