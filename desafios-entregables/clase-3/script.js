47c8e4bb3fd1a3d367cc63498b2a35edf55bebd0
Gastón A. Delucchi <gaston@MacBook-Pro-de-Gaston.local>
2023-03-26 10:52:00 -0300

    Aplicando cambios del after

--- desafios-entregables/clase-3/script.js
+++ desafios-entregables/clase-3/script.js
@@ -20,24 +20,18 @@ class ProductManager {
         }
 
         // Validaciones
-        if (
-            !product.title ||
+        if (!product.title ||
             !product.description ||
             !product.price ||
             !product.thumbnail ||
             !product.code ||
-            !product.stock
-        ) {
-            console.log(`Todos los campos son obligatorios`);
-        } else if (
-            this.products.some((stock) =>  stock.code === product.code)
-        ) {
-            console.log(`No se permiten códigos repetidos`);
-        } else {
-            this.products.push(product)
-            console.log(`Producto agregado correctamente`);
-        }
-    }
+            !product.stock) return `Todos los campos son obligatorios`;
+
+        let findCode = this.products.find(prod => prod.code === product.code)
+        if (findCode) return `No se permiten códigos repetidos`;
+        
+        return this.products.push(product)
+}
 
     // Método para mostrar los productos
     getProducts(){
@@ -54,20 +48,26 @@ class ProductManager {
 // Instancia de la clase
 const producto = new ProductManager()
 // Prueba del método getProducts
-producto.getProducts();
+console.log(producto.getProducts());
+
 // Prueba del método addProduct y revisión de correcto agregado al array
 producto.addProduct('Producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
 console.log(producto.getProducts());
+
 // Prueba del ID autoincrementable
-producto.addProduct('Producto prueba 2', 'Este es el segundo producto prueba', 100, 'Sin imagen', 'abc223', 1);
+producto.addProduct('Producto prueba 2', 'Este es el segundo producto prueba', 100, 'Sin imagen', 'bjd84', 1);
 console.log(producto.getProducts());
-// Prueba de validación de propiedades
-producto.addProduct('Producto prueba 3', 'Este es el tercer producto prueba', 50, 'Sin imagen', 'abc333');
+
+// Prueba de validación de propiedades (no stock)
+console.log(producto.addProduct('Producto prueba 3', 'Este es el tercer producto prueba', 50, 'Sin imagen', 'abc333'));
 console.log(producto.getProducts());
+
 // Prueba de validación de código repetido
-producto.addProduct('Producto prueba 4', 'Este es el cuarto producto prueba', 1000, 'Sin imagen', 'abc123', 3);
+console.log(producto.addProduct('Producto prueba 4', 'Este es el cuarto producto prueba', 1000, 'Sin imagen', 'abc123', 3));
 console.log(producto.getProducts());
+
 // Prueba del método getProductById
 console.log(producto.getProductById(2))
+
 // Forzamos la prueba del método getProductById
 console.log(producto.getProductById(5))
