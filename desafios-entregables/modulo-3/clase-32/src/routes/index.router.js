d8a5efbef8d7ddbce0e0dfd7ccd16218c8975c31
Sadax 25 <gdelucchi@me.com>
2023-07-24 20:53:32 -0300

    Changing Desafío Clase 32

--- desafios-entregables/modulo-3/clase-32/src/routes/index.router.js
+++ desafios-entregables/modulo-3/clase-32/src/routes/index.router.js
@@ -1,14 +1,12 @@
 // Imports
 const { Router } = require(`express`)
 const viewRouter = require(`./view.router`)
-const messageRouter = require(`./message.router`)
 const { uploader } = require("../utils/multer")
 const sessionRouter = require(`./session.router`)
 const UsersRouter = require('./user.routerGeneric')
 const TicketsRouter = require('./ticket.routerGeneric')
 const CartsRouter = require('./cart.routerGeneric')
 const ProductRouter = require("./product.routerGeneric")
-const pruebasRouter = require('./pruebas.router')
 const mockingProductsRouter = require('./mockingProducts.router')
 
 // Declaration
@@ -30,10 +28,6 @@ router.use(`/api/carts`, cartsRouter.getRouter())
 router.use('/api/tickets', ticketsRouter.getRouter())
 
 router.use(`/api/session`, sessionRouter)
-
-router.use(`/chat`, messageRouter)
-
-router.use(`/pruebas`, pruebasRouter)
 
 router.use(`/mockingproducts`, mockingProductsRouter)
 
--- desafios-entregables/modulo-3/clase-32/src/service/index.service.js
+++ desafios-entregables/modulo-3/clase-32/src/service/index.service.js
@@ -1,6 +1,4 @@
 const { UserDao, ProductDao, TicketDao, CartDao } = require("../dao/factory");
-const MessageDaoMongo = require("../dao/mongo/message.mongo");
-const OrderDaoMongo = require("../dao/mongo/order.mongo");
 const { CartRepository } = require("../repositories/cart.repository");
 const { ProductRepository } = require("../repositories/product.repository");
 const { TicketRepository } = require("../repositories/ticket.repository");
@@ -8,8 +6,6 @@ const { UserRepository } = require("../repositories/user.repository");
 
 const cartService = new CartRepository(new CartDao())
 const productService = new ProductRepository(new ProductDao())
-const messageService = new MessageDaoMongo()
-const orderService = new OrderDaoMongo()
 const ticketService = new TicketRepository(new TicketDao())
 const userService = new UserRepository(new UserDao())
 
@@ -17,7 +13,5 @@ module.exports = {
     userService,
     productService,
     cartService,
-    messageService,
-    orderService,
     ticketService
 }
